8230302783b2a154ad1e57bb39ab10732a3afa21
VntValenzuela <vincent.valenzuela@live.com>
2020-12-15 15:24:36 -0800

    Integrating Compras Module

---
+++ src/app/models/supplier.ts
@@ -0,0 +1,10 @@
+export interface Supplier {
+    count: any;
+    id: any;
+    correo?: string;
+    nombre: string;
+    numero: number;
+    NIT: number;
+    direccion: string;
+    password: string;
+  }
