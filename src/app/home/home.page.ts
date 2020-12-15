c454638daa88bbeb4d69ebbdf5d1d7b52ce72d13
Andres Gamboa Baldi <andresgamboabaldi@gmail.com>
2020-12-15 09:30:38 -0400

    Adding Connection to Firebase

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { FirestoreService } from "../services/firestore.service";
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+  motos: any = [];
+  constructor(
+    private firestoreService: FirestoreService
 
-  constructor() {}
+  ) { }
 
+  ngOnInit() {
+    this.getMotos();
+  }
+
+  getMotos() {
+    this.firestoreService.getData("Motos").subscribe((motosArray) => {
+      this.motos = [];
+      motosArray.forEach((moto: any) => {
+        console.log(moto.payload.doc.id);
+      });
+      console.log(this.motos);
+    });
+  }
 }
